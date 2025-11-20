54f72afcc619136f863453a856630af6d128edd5
Nathan Schram <5553883+nathanschram@users.noreply.github.com>
2025-11-20 17:41:10 +1100

    fix: add @octokit/request-error to Jest transformIgnorePatterns
    - ES module import error was preventing tests from running in CI
    - Added @octokit/request-error to transformIgnorePatterns so Jest can transform it
    - All 911 tests now passing locally
    - Fixes SyntaxError: Unexpected token 'export' in CI

--- jest.config.js
+++ jest.config.js
@@ -14,7 +14,9 @@ module.exports = {
     },
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  transformIgnorePatterns: ["node_modules/(?!(package-json|ky)/)"],
+  transformIgnorePatterns: [
+    "node_modules/(?!(package-json|ky|@octokit/request-error)/)",
+  ],
   verbose: true,
   clearMocks: true,
   resetMocks: true,
